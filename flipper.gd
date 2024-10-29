dcab2569e723ad1be47a40d239905c4203263d5d
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-10-29 13:00:42 -0300

    Fix boludez que habia comentado para mostrar algo en la oficina

--- flipper.gd
+++ flipper.gd
@@ -27,8 +27,8 @@ func _physics_process(delta):
 		delta * 10.0
 	)
 	var delta_rotation_percentage = rotation_percentage - previous_rotation_percentage
-	#if is_toggled() and delta_rotation_percentage > 0.0:
-		#constant_linear_velocity = Vector2.ZERO#Vector2.UP * 1000.0
-	#else:
-		#constant_linear_velocity = Vector2.ZERO
+	if is_toggled() and delta_rotation_percentage > 0.0:
+		constant_linear_velocity = Vector2.UP * 1000.0
+	else:
+		constant_linear_velocity = Vector2.ZERO
 	rotation = lerp_angle(original_rotation, rotation_when_toggled, rotation_percentage)
